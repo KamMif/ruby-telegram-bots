8dff32a2a83ad85439d2774b8d33592ae9a56e7a
Kam <kammif1990@gmail.com>
2018-06-07 18:48:17 +0500

    initial commit

---
+++ echo_bot/exho_bot.rb
@@ -0,0 +1,16 @@
+require 'telegram/bot'
+
+token = '542482305:AAHp1k_EoXn4-enr3fXSWcquIYELE8yrObA'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    else
+      bot.api.send_message(chat_id: message.chat.id, text: message)
+    end
+  end
+end
